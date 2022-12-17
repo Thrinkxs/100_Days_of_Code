e44a323a20afe990316c08a136f9c50be312b486
Thrinkxs <emmanuelozigue@gmail.com>
2022-12-17 23:12:11 +0200

    day 5 - #replit_100_days_of_code

---
+++ day_5.py
@@ -0,0 +1,16 @@
+catsOrDogs = input("Are you a cat person? Or a dog person?:")
+if catsOrDogs == "cat":
+    print("Meow")
+else:
+    print("Woof")
+
+print("Marvel Movie Character Creator")
+print("--")
+print()
+print(input("Do you like 'hanging around?'?: "))
+print("Then you're not Spiderman")
+print(input("Do yo have a 'gravelly' voice? "))
+print("Aww, you're not Korg")
+print()
+print(input("Do you often feel 'Marvelous'?: "))
+print("Aha! You're Captain Marvel! Hi!")
