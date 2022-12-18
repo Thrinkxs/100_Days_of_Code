497f2191dd641372031a57c6e653be6e12238096
Thrinkxs <emmanuelozigue@gmail.com>
2022-12-18 17:35:57 +0200

    day 6 - #replit_100_days_of_code

---
+++ day_6.py
@@ -0,0 +1,27 @@
+print("SECURE LOGIN")
+username = input("Username > ")
+password = input("Password> ")
+if username == "mark" and password == "password":
+  print("Welcome Mark!")
+elif username == "Emmanuel" and password == "emmyrox":
+  print("Welcome Emmanuel")
+else:
+  print("Go away!")
+
+print("My LOGiN SYSTEM")
+print("******************")
+username = input("Enter username: ")
+password = input("Enter password: ")
+if username == "David" and password == "Dave40":
+  print(f"Why hello there {username}, what a lovely accent you have, you could have charmed your way in here without a password")
+  print()
+  print("Have a great day")
+  print("Don't forget to wear a hat in the sun!")
+elif username == "Emmanuel" and password == "Emmy":
+  print(f"Why hello there {username}, what a lovely accent you have, you could have charmed your way in here without a password")
+  print()
+  print("Have a great day")
+  print("Don't forget to wear a hat in the sun!")
+  print()
+else:
+  print("Who are you stranger?!")
